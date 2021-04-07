11549f67689588b61a143b10ad20ca8f4b148bfc
Li Jeffery <Jeffery.Li@dematic.com>
2021-04-07 15:43:37 -0400

    Added Initial Unity Project

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    private Vector3 direction = Vector3.right;
+    public float speed = 5;
+    public float maxX; // x value that the block is to be destroyed at
+    
+    void Update()
+    {
+        transform.Translate(Time.deltaTime * direction);
+        if (transform.x > maxX)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/BlockManager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class BlockManager : MonoBehaviour
+{
+    public Vector3 spawnBoxPoint1 = new Vector3(-6, 6, 6);
+    public Vector3 spawnBoxPoint2 = new Vector3(-7, -6, -6);
+    public int spawnsPerSecond = 10;
+    public GameObject blockPrefab;
+    public float destroyX = 5; // X value that blocks are to be destroyed at
+    private float _maxX, _minX, _maxY, _minY, _maxZ, _minZ;
+    private bool _isBlockPrefabNotNull;
+    private float _secondsPerSpawn;
+
+    void Start()
+    {
+        _isBlockPrefabNotNull = blockPrefab != null;
+        _secondsPerSpawn = 1 / spawnsPerSecond;
+        CalculateMinMaxSpawnVectors();
+        StartCoroutine(SpawnBlocks());
+    }
+
+    IEnumerator SpawnBlocks()
+    {
+        while (true)
+        {
+            SpawnOneBlock();
+            yield return new WaitForSeconds(_secondsPerSpawn);
+        }
+    }
+
+    void SpawnOneBlock()
+    {
+        var spawnPosition = GetPositionInSpawnBox();
+        if (_isBlockPrefabNotNull)
+        {
+            var blockGO = Instantiate(blockPrefab, spawnPosition, Quaternion.identity);
+            var block = blockGO.GetComponent<Block>();
+            block.maxX = destroyX;
+        }
+    }
+    
+    #region Helper Methods
+    void CalculateMinMaxSpawnVectors()
+    {
+        var p1 = spawnBoxPoint1;
+        var p2 = spawnBoxPoint2;
+        _maxX = Mathf.Max(p1.x, p2.x);
+        _minX = Mathf.Min(p1.x, p2.x);
+        _maxY = Mathf.Max(p1.y, p2.y);
+        _minY = Mathf.Min(p1.y, p2.y);
+        _maxZ = Mathf.Max(p1.z, p2.z);
+        _minZ = Mathf.Min(p1.z, p2.z);
+    }
+
+    Vector3 GetPositionInSpawnBox()
+    {
+        var p1 = spawnBoxPoint1;
+        var p2 = spawnBoxPoint2;
+        return new Vector3(
+            x: Random.Range(_minX, _maxX),
+            y: Random.Range(_minY, _maxY),
+            z: Random.Range(_minZ, _maxZ)
+        );
+    }
+    #endregion
+}
